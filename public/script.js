6291f1a661022f2dca33c22cb2be6240cb65fc94
rmirmotahari <49576793+rmirmotahari@users.noreply.github.com>
2020-11-07 17:26:26 -0500

    Updates to the Javascript, creation of display function
    - CSS: Added styling to the list elements for search results
    - Javascript: Added a display function, refined the display matches function.
    - Added event listener functions for search.
    
    Javascript is still not complete but at the moment it will load up search queries. Styling will need to be refined, (i.e new fonts etc).

--- public/script.js
+++ public/script.js
@@ -1,17 +1,42 @@
 
 const restaurant = [];
+
 fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
   .then(response => response.json())
-  .then(data => restaurant.push(...data))
+  .then(data => restaurant.push(...data));
+ 
 
   function findMatches(wordToMatch, restaurant){
     return restaurant.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi');
-        return place.name.match(regex) || place.city.match(regex)
-    });
+        return place.name.match(regex) || place.city.match(regex);
+    })
 
 }
 
+function displayArray() {
+    return restaurant;
+}
 
+function displayMatches() {
+    const matchArray = findMatches(this.value, restaurant);
+    const html = matchArray.map(place => {
+        return `
+            <li>
+                <span class="name">${place.name}</span>
+                <span class="address">${place.address}</span>
+                <span class="category">${place.category}</span>
+            </li>
+            `
+    }).join('');
+    suggestions.innerHTML = html;
+    console.log(matchArray);
+}
+
+const searchInput = document.querySelector('.textentry');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
 
 
